60a28bf3445a5ad866e0ba5f3cda9a2f8b44997a
juaking117 <joaquin.espinoza15@inacapmail.cl>
2024-11-14 10:40:56 -0300

    1.1

--- login14nov/logearse/models.py
+++ login14nov/logearse/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class login():
+    usuario_nombre = models.CharField(max_length=100,verbose_name='Usuario')
+    usuario_password = models.CharField(max_length=100,verbose_name='Password')
