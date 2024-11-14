6f8c31630d1cc9d3eafb1468501e38532e41bbbe
InaLiwi <paula.zelada@inacapmail.cl>
2024-11-14 10:51:55 -0300

    ejemplo de views

--- login14nov/logearse/views.py
+++ login14nov/logearse/views.py
@@ -10,3 +10,34 @@ def inicio(request):
 
 def login(request):
     return render(request, 'paginas/login.html')
+
+
+# USUARIOS:
+
+
+def reservas(request):
+    reservas = Reserva.objects.all()
+    return render(request, 'reservas/index.html', {'reservas': reservas})
+
+def crear_reserva(request):
+    formulario = ReservaForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('reservas')
+    return render(request, 'reservas/crear.html', {'formulario':formulario})
+
+def editar_reserva(request, reserva_id):
+    reserva = get_object_or_404(Reserva, reserva_id=reserva_id)
+    if request.method == 'POST':
+        formulario = ReservaForm(request.POST, request.FILES, instance=reserva)
+        if formulario.is_valid():
+            formulario.save()  
+            return redirect('reservas')  
+    else:
+        formulario = ReservaForm(instance=reserva)  
+    return render(request, 'reservas/editar.html', {'formulario': formulario})
+
+def eliminar_reserva(request, reserva_id):
+    reserva = Reserva.objects.get(reserva_id = reserva_id)
+    reserva.delete()
+    return redirect('reservas')
