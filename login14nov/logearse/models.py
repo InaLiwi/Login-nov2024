e3314ca092a8531fc5dbe3a406efbc3b34d925da
InaLiwi <paula.zelada@inacapmail.cl>
2024-11-14 10:59:23 -0300

    usuarios

--- login14nov/logearse/models.py
+++ login14nov/logearse/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class login():
     usuario_nombre = models.CharField(max_length=100,verbose_name='Usuario')
     usuario_password = models.CharField(max_length=100,verbose_name='Password')
+
+class Usuario:
+    usuario_nombre = models.CharField(max_length=100,verbose_name='Usuario'),
+    usuario_password = models.CharField(max_length=100,verbose_name='Password'),
+    #usuario_rol = models.IntegerField(max_length=100,verbose_name='Rol'),
